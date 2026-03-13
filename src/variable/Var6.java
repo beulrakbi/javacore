d797ab449e1010e9e8d9c0b8fbd58ec89325aec4
beulrakbi <iamjyh99@gmail.com>
2026-03-13 18:14:53 +0900

    변수

--- src/variable/Var6.java
+++ src/variable/Var6.java
@@ -6,6 +6,7 @@ public class Var6 {
 //        System.out.println(a); 변수를 초기화 하지 않고 사용하면 에러가 발생한다.
         a = 1;
         System.out.println(a);
+        System.out.println(a);
 
         //컴파일 에러 컴파일러가 초기화 안된 a를 잡아냄
     }
